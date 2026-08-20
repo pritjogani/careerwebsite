1a5f770cdc31faa63aa058452a72304e914acf7d
pritjogani <joganiprit2004@gmail.com>
2026-08-20 18:25:33 +0530

    code solve

--- server/router/jobservice.js
+++ server/router/jobservice.js
@@ -9,6 +9,7 @@ router.route("/addjob").post(usermiddleware,hrmiddleware,authjob.addJob)
 router.route("/updatejob/:id").put(usermiddleware, hrmiddleware,authjob.updatejobinfo) //testing is not done
 router.route("/deletejob/job/:id").delete(usermiddleware, hrmiddleware, authjob.deletejob) //testing is not done
 router.route("/alljobtitle").get(authjob.getAlljobtitle)
+router.route("/alljobtitle").get(authjob.getAlljobtitle)
 
 
 module.exports = router;
